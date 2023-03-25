5315150ee9347e8300274accb76c7c7097cae76b
mamad <mamad@mamad.mamad>
2023-03-25 12:54:09 +0330

    Initial commimt

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+sudo apt update && sudo apt upgrade
+
+sudo apt install \
+     alacritty \
+     build-essential \
+     awesome \
+     rofi \
+     polybar \
+     cmatrix \
+     curl \
+     emacs \
+     feh \
+     git \
+     htop \
+     i3 \
+     lynx \
+     mpv \
+     mu \
+     neofetch \
+     pandoc \
+     pass \
+     picom \
+     proxychains \
+     rsync \
+     stow \
+     texlive-base \
+     texlive-fonts-recommended \
+     texlive-latex-base \
+     tree \
+     vim \
+     wget \
+     wine \
+     zathura \
+     zsh \
